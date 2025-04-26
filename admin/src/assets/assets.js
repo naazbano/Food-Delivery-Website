08e4f77a48ebc8ee9643b73ce5df57f49e0c5e22
naazbano <165495553+naazbano@users.noreply.github.com>
2025-04-26 12:13:09 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-website-back-op24.onrender.com'
+export const url = "https://food-delivery-website-back-op24.onrender.com";
